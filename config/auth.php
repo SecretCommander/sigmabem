5964a82fbf4200b1107bb626e963f01a976bb0a9
ChairawanChannel <muhammadchairawan99@gmail.com>
2026-07-08 22:18:34 +0700

    Bikin tabel baru di database untuk menyimpan data user (pengguna), cache (penyimpanan sementara), dan antrean tugas (jobs).

--- config/auth.php
+++ config/auth.php
@@ -66,6 +66,11 @@ return [
             'driver' => 'eloquent',
             'model' => env('AUTH_MODEL', User::class),
         ],
+
+        // 'users' => [
+        //     'driver' => 'database',
+        //     'table' => 'users',
+        // ],
     ],
 
     /*
@@ -73,16 +78,24 @@ return [
     | Resetting Passwords
     |--------------------------------------------------------------------------
     |
-    | FITUR RESET PASSWORD DINONAKTIFKAN
-    | krna sistem belum menggunakan fitur "Lupa Password".
-    | akan diaktifkan kembali saat fitur tersebut diimplementasikan.
+    | These configuration options specify the behavior of Laravel's password
+    | reset functionality, including the table utilized for token storage
+    | and the user provider that is invoked to actually retrieve users.
+    |
+    | The expiry time is the number of minutes that each reset token will be
+    | considered valid. This security feature keeps tokens short-lived so
+    | they have less time to be guessed. You may change this as needed.
+    |
+    | The throttle setting is the number of seconds a user must wait before
+    | generating more password reset tokens. This prevents the user from
+    | quickly generating a very large amount of password reset tokens.
     |
     */
 
     'passwords' => [
         'users' => [
             'provider' => 'users',
-            'table' => 'password_reset_tokens',  // status nya disable
+            'table' => env('AUTH_PASSWORD_RESET_TOKEN_TABLE', 'password_reset_tokens'),
             'expire' => 60,
             'throttle' => 60,
         ],
